5b1a5abd53f70eb4889f6e5e662327aa16a03c7d
Denesh neupane <ecedenesh.203@gmail.com>
2024-03-06 16:18:53 +0900

    Add files via upload

---
+++ beep/beep/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace beep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Check = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Comment = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Check
+            // 
+            this.Check.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Check.Location = new System.Drawing.Point(9, 121);
+            this.Check.Name = "Check";
+            this.Check.Size = new System.Drawing.Size(95, 35);
+            this.Check.TabIndex = 0;
+            this.Check.Text = "Enter";
+            this.Check.UseVisualStyleBackColor = true;
+            this.Check.Click += new System.EventHandler(this.Check_Click);
+            // 
+            // Comment
+            // 
+            this.Comment.Location = new System.Drawing.Point(95, 69);
+            this.Comment.Name = "Comment";
+            this.Comment.Size = new System.Drawing.Size(126, 19);
+            this.Comment.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.Check);
+            this.panel1.Controls.Add(this.Comment);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 171);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(139, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(32, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter  Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(18, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 195);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Detect USB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Check;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox Comment;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ beep/beep/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Linq;
+using System.Management;
+using System.Threading;
+using System.Xml.Linq;
+
+namespace beep
+{
+    public partial class Form1 : Form
+    {
+        private ManagementEventWatcher watcher;
+        private bool usbInserted;
+        private Thread beepingThread;
+        public Form1()
+        {
+            InitializeComponent();
+            usbInserted = false;
+            beepingThread = new Thread(ContinuousBeep);
+            this.Visible = false;
+            this.Opacity = 0;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WqlEventQuery query = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 2");
+
+            // Initialize the watcher and subscribe to the event
+            watcher = new ManagementEventWatcher(query);
+            watcher.EventArrived += new EventArrivedEventHandler(DeviceInsertedEvent);
+            watcher.Start();
+
+            
+        }
+        private void DeviceInsertedEvent(object sender, EventArrivedEventArgs e)
+        {
+            // Console.Beep();
+            if (!usbInserted)
+            {
+                usbInserted = true;
+                this.Invoke(new MethodInvoker(delegate
+                {
+                    this.Visible = true; // Show the form when beep occurs
+                    this.Opacity = 1;
+                }));
+                if (!beepingThread.IsAlive)
+                {
+                    beepingThread = new Thread(ContinuousBeep);
+                    beepingThread.Start();
+                }
+            }
+        }
+        private void ContinuousBeep()
+        {
+            // Continuously beep until the USB device is removed
+            while (usbInserted)
+            {
+                Console.Beep();
+                // Adjust the beep duration or frequency if needed
+                Thread.Sleep(1000); // Beep every second
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (watcher != null)
+            {
+                watcher.Stop();
+                watcher.Dispose();
+            }
+
+            // Signal the beeping thread to stop
+            usbInserted = false;
+
+            // Wait for the beeping thread to complete
+            if (beepingThread.IsAlive)
+            {
+                beepingThread.Join();
+            }
+        }
+        private void Check_Click(object sender, EventArgs e)
+        {
+            if (Comment.Text == "12345")
+            {
+                // Stop the continuous beep
+                usbInserted = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
